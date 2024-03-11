cb6c287c451101024ae57fc4bacdc70f323860d5
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2024-03-11 21:51:26 +0900

    Add paging spec

--- app/actions/books/index.rb
+++ app/actions/books/index.rb
@@ -5,6 +5,11 @@ module Bookshelf
     module Books
       class Index < Bookshelf::Action
         def handle(request, response)
+          response.render(
+            view,
+            page: request.params[:page] || 1,
+            per_page: request.params[:per_page] || 5
+          )
         end
       end
     end
--- app/views/books/index.rb
+++ app/views/books/index.rb
@@ -6,10 +6,12 @@ module Bookshelf
       class Index < Bookshelf::View
         include Deps["persistence.rom"]
 
-        expose :books do
+        expose :books do |page:, per_page:|
           rom.relations[:books]
             .select(:title, :author)
             .order(:title)
+            .page(page)
+            .per_page(per_page)
             .to_a
         end
       end
