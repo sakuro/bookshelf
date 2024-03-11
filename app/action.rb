e7ac2b1054524c8b7a9fa0de3eb492f9ee0e1b56
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2024-03-11 22:07:59 +0900

    Move 404 handler to Action

--- app/action.rb
+++ app/action.rb
@@ -5,5 +5,12 @@ require "hanami/action"
 
 module Bookshelf
   class Action < Hanami::Action
+    handle_exception ROM::TupleCountMismatchError => :handle_not_found
+
+    private def handle_not_found(request, response, exception)
+      response.status = 404
+      response.format = :html
+      response.body = "Not found"
+    end
   end
 end
--- app/actions/books/show.rb
+++ app/actions/books/show.rb
@@ -4,8 +4,6 @@ module Bookshelf
   module Actions
     module Books
       class Show < Bookshelf::Action
-        handle_exception ROM::TupleCountMismatchError => :handle_not_found
-
         params do
           required(:id).value(:integer)
         end
@@ -15,12 +13,6 @@ module Bookshelf
             view,
             id: request.params[:id]
           )
-        end
-
-        private def handle_not_found(request, response, exception)
-          response.status = 404
-          response.format = :html
-          response.body = "Not found"
         end
       end
     end
