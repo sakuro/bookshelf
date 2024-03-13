4b41a07dbe65227f27713b5799946dead11a3ba1
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2024-03-13 20:51:25 +0900

    Make #call and #[] function identically

--- app/operation.rb
+++ app/operation.rb
@@ -6,5 +6,16 @@ require "dry/monads"
 module Bookshelf
   class Operation
     include Dry::Monads[:result]
+
+    # Ensures that #call and #[] function identically
+    def self.method_added(method_name)
+      if method_name == :call && !instance_methods.include?(:[])
+        alias_method :[], :call
+      elsif method_name == :[] && !instance_methods.include?(:call)
+        alias_method :call, :[]
+      end
+
+      super
+    end
   end
 end
