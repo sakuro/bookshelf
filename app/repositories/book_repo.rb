9caa92312eb56e84a6891dc62d97d834c165f025
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2024-03-13 22:28:21 +0900

    Add basic interface

--- app/repositories/book_repo.rb
+++ app/repositories/book_repo.rb
@@ -7,7 +7,19 @@ module Bookshelf
     class BookRepo < ROM::Repository[:books]
       include Deps[container: "persistence.rom"]
 
-      commands :create
+      commands :create, update: :by_pk, delete: :by_pk
+
+      def by_id(id)
+        books.by_pk(id).one!
+      end
+
+      def all
+        books.to_a
+      end
+
+      def count
+        books.count
+      end
     end
   end
 end
