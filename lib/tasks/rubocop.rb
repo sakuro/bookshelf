d612cd5ff528e556c5c3d107fc3abaa3e42ae1a6
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2024-03-15 22:10:52 +0900

    Avoid anonymous module

--- lib/tasks/rubocop.rb
+++ lib/tasks/rubocop.rb
@@ -2,24 +2,31 @@
 
 require "rubocop/rake_task"
 
-RuboCop::RakeTask.prepend(Module.new {
-  def setup_subtasks(name, *args, &task_block)
-    super
+module RuboCop
+  class RakeTask
+    module AddRegenerateToDo
+      def setup_subtasks(name, *args, &task_block)
+        super
 
-    namespace(name) do
-      desc "Regenerate RuboCop's TooDo"
-      task(:regenerate_todo, *args) do |_, task_args|
-        RakeFileUtils.verbose(verbose) do
-          yield(*[self, task_args].slice(0, task_block.arity)) if task_block
-          perform("--regenerate-todo")
+        namespace(name) do
+          desc "Regenerate RuboCop's TooDo"
+          task(:regenerate_todo, *args) do |_, task_args|
+            RakeFileUtils.verbose(verbose) do
+              yield(*[self, task_args].slice(0, task_block.arity)) if task_block
+              perform("--regenerate-todo")
+            end
+          end
         end
       end
     end
-  end
-})
 
-RuboCop::RakeTask.new(:rubocop) do |task|
-  task.options = %w[--display-cop-names --display-style-guide --extra-details]
-  # Use Rubocop's Github Actions formatter if possible
-  task.formatters << "github" if ENV["GITHUB_ACTIONS"] == "true"
+    private_constant :AddRegenerateToDo
+    prepend AddRegenerateToDo
+
+    new(:rubocop) do |task|
+      task.options = %w[--display-cop-names --display-style-guide --extra-details]
+      # Use Rubocop's Github Actions formatter if possible
+      task.formatters << "github" if ENV["GITHUB_ACTIONS"] == "true"
+    end
+  end
 end
