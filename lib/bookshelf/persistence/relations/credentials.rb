5b02b1c591b483ea797d2d21a4e066d71a4f5ed4
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2024-03-21 18:18:57 +0900

    Include transformer inside relation

--- lib/bookshelf/persistence/relations/credentials.rb
+++ lib/bookshelf/persistence/relations/credentials.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
+require "dry-transformer"
+
 module Bookshelf
   module Persistence
     module Relations
       class Credentials < ROM::Relation[:sql]
+        module T
+          extend Dry::Transformer::Registry
+
+          import :deep_symbolize_keys, from: Dry::Transformer::HashTransformations
+        end
+
         schema(:credentials, infer: true) do
           attribute :data,
             ROM::SQL::Postgres::Types::JSONB,
-            read: ROM::SQL::Postgres::Types::JSONB.constructor(:to_h.to_proc >> Types::RecursivelySymbolizeKeys)
+            read: ROM::SQL::Postgres::Types::JSONB.constructor(:to_h.to_proc >> T[:deep_symbolize_keys])
           associations do
             belongs_to :account
           end
--- lib/bookshelf/types.rb
+++ lib/bookshelf/types.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require "dry/transformer"
 require "dry/types"
 
 module Bookshelf
@@ -8,16 +7,5 @@ module Bookshelf
 
   module Types
     # Define your custom types here
-    module Transforms
-      extend Dry::Transformer::Registry
-
-      import Dry::Transformer::HashTransformations
-      import Dry::Transformer::Recursion
-    end
-
-    private_class_method def self.t(*) = Transforms[*]
-
-    RecursivelySymbolizeKeys = t(:hash_recursion, t(:symbolize_keys))
-    public_constant :RecursivelySymbolizeKeys
   end
 end
