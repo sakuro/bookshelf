cc0003549dac6a7b6e6229d5d95714b2f01f1263
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2024-03-22 23:00:32 +0900

    Disallow null

--- db/migrate/20240320082019_create_credentials.rb
+++ db/migrate/20240320082019_create_credentials.rb
@@ -4,7 +4,7 @@ ROM::SQL.migration do
   change do
     create_table :credentials do
       primary_key :id
-      foreign_key :account_id, :accounts
+      foreign_key :account_id, :accounts, null: false
       column :type, :text, null: false
       column :data, :jsonb, null: false
       unique %i[account_id type]
